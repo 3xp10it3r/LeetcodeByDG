0b8cef28690767edba9191eb64c55b0d80d9fa54
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2025-06-15 19:49:29 +0530

    Time: 30 ms (81.24%) | Memory: 61.6 MB (98.52%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,14 +1,13 @@
 class Solution {
-private:
-    static int getDistance(vector<int>& v) {
-        return v[0] * v[0] + v[1] * v[1];
-    }
 public:
     
+    int getDistance(vector<int>& v) {
+        return v[0] * v[0] + v[1] * v[1];
+    }
 
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         
-        sort(points.begin(), points.end(), [](vector<int>& a, vector<int>& b){
+        sort(points.begin(), points.end(), [&](vector<int>& a, vector<int>& b){
             return getDistance(a) < getDistance(b);
         });
 

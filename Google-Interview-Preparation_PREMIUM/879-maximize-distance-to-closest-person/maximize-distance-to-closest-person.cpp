171fc8d2728998913f9ad57f86302ef0c25cc9ee
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2025-06-17 01:16:21 +0530

    Time: 1 ms (25.12%) | Memory: 23 MB (55.38%) - LeetSync

--- Google-Interview-Preparation_PREMIUM/879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.cpp
+++ Google-Interview-Preparation_PREMIUM/879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.cpp
@@ -10,18 +10,18 @@ public:
                 zeroCount = 0;
             } else {
                 zeroCount++;
-                ans = max(ans, (zeroCount + 1) / 2);
+                ans = max(ans, (zeroCount + 1) / 2); // 1 000 1  (3 + 1)/ 2 = 2
             }
         }
 
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++) { // left 0's with no left boundary
             if(seats[i] == 1) {
                 ans = max(ans, i);
                 break;
             }
         }
 
-        for(int i = n-1; i >= 0; i--) {
+        for(int i = n-1; i >= 0; i--) { // right 0's with no right boundary
             if(seats[i] == 1) {
                 ans = max(ans, n - i - 1);
                 break;

b9903b4d3c4b4fd54dabd5d4449a48843a91c994
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2025-06-16 20:14:03 +0530

    Time: 9 ms (59.45%) | Memory: 11.4 MB (83.86%) - LeetSync

--- Google-Interview-Preparation_PREMIUM/76-minimum-window-substring/minimum-window-substring.cpp
+++ Google-Interview-Preparation_PREMIUM/76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,49 +1,55 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-
-
-        unordered_map<char,int> mp;
-
-        for(auto ch: t) mp[ch]++;
-
-        int len = t.size();
-
+        
         int n = s.size();
+        int len = t.size();
+        
         int head = -1, tail = 0;
-
-        int startIndex = -1, length = 1e9;
-
+        
+        int startIndex = -1;
+        int ansLength = 1e9;
+        
+        
+        //ds 
+        unordered_map<char, int> mp;
+        
         int cnt = 0;
+        
+        //ds initial update
+        
+        for(auto &ch: t) {
+            mp[ch]++;
+        }
+        
         while(head < n) {
-            while(head + 1 < n && cnt < len){
+            while(head + 1 < n && cnt < len) {
                 head++;
+                
                 if(mp[s[head]] > 0) {
                     cnt++;
-                } 
+                }
+                
                 mp[s[head]]--;
             }
-            // cout << "t : " << tail << "h : " << head << endl;
-            if(cnt == len && (head - tail + 1) < length) {
+            
+            if(cnt == len && (head - tail + 1) < ansLength) {
                 startIndex = tail;
-                length = head - tail + 1;
+                ansLength = head - tail + 1;
             }
-
+            
             if(tail > head) {
-                tail++;
-                head = tail - 1;
+                head = tail;
+                tail = head + 1;
             } else {
-                //remove from ds
                 mp[s[tail]]++;
                 if(mp[s[tail]] > 0) {
                     cnt--;
                 }
-                // mp[s[tail]]--;
                 tail++;
             }
         }
-
         if(startIndex == -1) return "";
-        return s.substr(startIndex, length);
+        return s.substr(startIndex, ansLength);
     }
 };

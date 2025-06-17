ed90e80b5f4a07732d625f2a6f214aa650df4f03
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2025-06-17 15:10:27 +0530

    Time: 3 ms (72.22%) | Memory: 13.4 MB (77.22%) - LeetSync

--- Google-Interview-Preparation_PREMIUM/22-generate-parentheses/generate-parentheses.cpp
+++ Google-Interview-Preparation_PREMIUM/22-generate-parentheses/generate-parentheses.cpp
@@ -3,43 +3,34 @@ public:
     string s;
     vector<string> ans;
 
-    bool isBalanced(string& s) {
-        int cnt = 0;
-
-        for(int i = 0; i < s.size(); i++) {
-            if(s[i] == '(') cnt++;
-            else {
-                if(cnt <= 0) return false;
-                cnt--;
-            }
-        }
-
-        return cnt == 0;
-    }
-
-    void helper(int n) {
-        if(n == 0) {
-            if(isBalanced(s)) {
-                ans.push_back(s);
-            }
+    void helper(int n, int open, int close) {
+        if(open + close  ==  2 * n) {
+           
+            ans.push_back(s);
             return;
         }
 
-        s += "(";
+        if(open < n) {
 
-        helper( n - 1 );
+            s += "(";
 
-        s.pop_back();
+            helper(n, open + 1, close);
 
-        s += ")";
+            s.pop_back();
+        } 
+    
+        if(close < open) {
+            s += ")";
 
-        helper(n-1);
+            helper(n, open, close + 1);
 
-        s.pop_back();
+            s.pop_back();
+        }
+      
     }    
     vector<string> generateParenthesis(int n) {
         s = "";
-        helper(2 * n);
+        helper(n, 0, 0);
         return ans;
     }
 };

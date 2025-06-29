088f37789b85a60f203397a7a663f0ef8067a25d
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2025-06-30 03:48:00 +0530

    Time: 1 ms (94.47%) | Memory: 8.3 MB (97.00%) - LeetSync

--- Google-Interview-Preparation_PREMIUM/576-out-of-boundary-paths/out-of-boundary-paths.cpp
+++ Google-Interview-Preparation_PREMIUM/576-out-of-boundary-paths/out-of-boundary-paths.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int m, n;
-    long long dp[51][51][51];
+    int dp[51][51][51];
     const int MOD = 1e9 + 7;
 
-    long long helper(int i, int j, int maxMove) {
+    int helper(int i, int j, int maxMove) {
         if(i >= m || j >= n || i < 0 || j < 0) return 1;
 
         if(maxMove == 0) return 0;   
@@ -12,10 +12,10 @@ public:
         if(dp[i][j][maxMove] != -1) return dp[i][j][maxMove];
 
         return dp[i][j][maxMove] = (
-            helper(i + 1, j, maxMove - 1) % MOD +
-            helper(i - 1, j, maxMove - 1) % MOD +
-            helper(i, j + 1, maxMove - 1) % MOD +
-            helper(i, j - 1, maxMove - 1) % MOD
+            (helper(i + 1, j, maxMove - 1) +
+            helper(i - 1, j, maxMove - 1)) % MOD +
+            (helper(i, j + 1, maxMove - 1)  +
+            helper(i, j - 1, maxMove - 1)) % MOD
         ) % MOD;
     }
 

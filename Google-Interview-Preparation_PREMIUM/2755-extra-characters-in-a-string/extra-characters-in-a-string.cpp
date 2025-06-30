a71feeef2b80a9b297d93c5075f7a55b354b3b2e
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2025-06-30 19:45:18 +0530

    Time: 55 ms (79.70%) | Memory: 71.8 MB (76.30%) - LeetSync

---
+++ Google-Interview-Preparation_PREMIUM/2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int helper(int level, string& s, unordered_map<string, int>& mp, vector<int>& dp) {
+        if(level >= s.size()) return 0;
+
+        if(dp[level] != -1) return dp[level];
+
+        string curStr = "";
+
+        int ans = s.size();
+
+        for(int i = level; i < s.size(); i++) {
+            curStr.push_back(s[i]);
+
+            int count = ((mp.count(curStr)) ? 0: curStr.size()) 
+                        + helper(i + 1, s, mp, dp); 
+            ans = min(ans, count);
+        }
+
+        return dp[level] = ans;
+    }
+
+    int minExtraChar(string s, vector<string>& dictionary) {
+        vector<int> dp(s.size(), -1);
+
+        unordered_map<string, int> mp;
+
+        for(string &word: dictionary) mp[word]++;
+
+        return helper(0, s, mp, dp);
+    }
+};

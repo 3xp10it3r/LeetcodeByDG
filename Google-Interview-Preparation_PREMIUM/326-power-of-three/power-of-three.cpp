771410feb0e2df2804ba29247e7d25067fa383ea
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2025-08-13 22:07:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (76.82%) - LeetSync

--- Google-Interview-Preparation_PREMIUM/326-power-of-three/power-of-three.cpp
+++ Google-Interview-Preparation_PREMIUM/326-power-of-three/power-of-three.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n <= 0) return false;
-        while (n % 3 == 0) {
-            n /= 3;
-        }
-        return n == 1;
+        return n > 0 && 1162261467 % n == 0; // 3^19 largest power of 3 such that it is inside integer.
     }
 };
-
-// n = 3^x
-
-// log n = x log 3

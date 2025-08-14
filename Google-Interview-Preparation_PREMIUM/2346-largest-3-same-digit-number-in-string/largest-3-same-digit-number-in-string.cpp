b755246d5fc8e34a1fa5c82b7e75ee8fcc341375
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2025-08-14 10:00:44 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (52.45%) - LeetSync

--- Google-Interview-Preparation_PREMIUM/2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ Google-Interview-Preparation_PREMIUM/2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     string largestGoodInteger(string num) {
         int n = num.size();
-        string ans = "";
+        char mxDigit = '\0';
         for(int i = 0; i <= n - 3; i++) {
-            string sub = num.substr(i, 3);
-            if(sub[0] == sub[1] && sub[1] == sub[2]) {
-                if(ans < sub) ans = sub;
+            if(num[i] == num[i+1] && num[i+1] == num[i+2]) {
+                mxDigit = max(mxDigit, num[i]);
             }
         }
 
-        return ans;
+        return mxDigit == '\0' ? "" : string(3, mxDigit);
     }
 };

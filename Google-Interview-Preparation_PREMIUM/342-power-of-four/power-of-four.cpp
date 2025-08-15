236314ba2c91e06f993977860b4f503af6fefbbf
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2025-08-16 00:34:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (52.41%) - LeetSync

--- Google-Interview-Preparation_PREMIUM/342-power-of-four/power-of-four.cpp
+++ Google-Interview-Preparation_PREMIUM/342-power-of-four/power-of-four.cpp
@@ -2,11 +2,15 @@ class Solution {
 public:
     bool isPowerOfFour(int n) {
         if(n <= 0) return 0;
-        
-        while(n % 4 == 0) {
-            n /= 4;
+
+        bitset<31> bits(n);
+
+        if(bits.count() != 1) return 0;
+
+        for(int i = 0; i < 31; i+=2) {
+            if(bits[i] == 1) return 1;
         }
 
-        return n == 1;
+        return 0;
     }
 };

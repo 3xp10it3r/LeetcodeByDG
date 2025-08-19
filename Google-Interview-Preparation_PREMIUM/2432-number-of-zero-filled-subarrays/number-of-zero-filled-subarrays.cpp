2ea7c8061f381158e688a62d486567507b314809
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2025-08-19 21:56:52 +0530

    Time: 0 ms (100.00%) | Memory: 111.4 MB (11.10%) - LeetSync

--- Google-Interview-Preparation_PREMIUM/2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
+++ Google-Interview-Preparation_PREMIUM/2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
@@ -4,19 +4,19 @@ public:
         int n = nums.size();
         long long ans = 0;
 
-        long long curLen = 0;
+        int curLen = 0;
         
         for(int i = 0; i < n; i++) {
             if(nums[i] == 0) curLen++;
             else {
                 if(curLen)
-                ans += curLen * (curLen + 1) / 2;
+                ans += 1LL * curLen * (curLen + 1) / 2;
                 curLen = 0;
             }
         }
 
         if(curLen) {
-            ans += curLen * (curLen + 1) / 2;
+            ans += 1LL * curLen * (curLen + 1) / 2;
         }
 
         return ans;

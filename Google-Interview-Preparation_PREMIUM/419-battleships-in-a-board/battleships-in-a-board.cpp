5578e0b5066e3f9211ae47cc7d1a32bbcecb78ec
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2025-09-20 00:23:18 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (96.87%) - LeetSync

--- Google-Interview-Preparation_PREMIUM/419-battleships-in-a-board/battleships-in-a-board.cpp
+++ Google-Interview-Preparation_PREMIUM/419-battleships-in-a-board/battleships-in-a-board.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
-    vector<int> dx = {-1, 1, 0, 0};
-    vector<int> dy = {0, 0, -1, 1};
-
-    void dfs(int i, int j, int n, int m, vector<vector<char>>& board, vector<vector<int>>& vis) {
-        vis[i][j] = 1;
-        for(int k = 0; k < 4; k++) {
-            int ni = i + dx[k];
-            int nj = j + dy[k];
-
-            if(ni >= 0 && nj >= 0 && ni < n && nj < m && !vis[ni][nj] && board[ni][nj] == 'X') {
-                dfs(ni, nj, n, m, board, vis);
-            }
-        }
-    }
 
     int countBattleships(vector<vector<char>>& board) {
         int n = board.size(), m = board[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, 0));
 
         int ans = 0;
 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(!vis[i][j] && board[i][j] == 'X') {
-                    dfs(i, j, n, m, board, vis);
-                    ans++;
-                }
+                if(i - 1 >= 0 && board[i-1][j] == 'X') continue;
+                if(j - 1 >= 0 && board[i][j-1] == 'X') continue;
+
+                if(board[i][j] == 'X')
+                ans++;
             }
         }
 

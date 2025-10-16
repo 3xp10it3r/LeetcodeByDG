1078f25ec6f4c36ded49c2eff8251fe52be2f792
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2025-10-16 23:48:39 +0530

    Time: 45 ms (5.24%) | Memory: 103 MB (66.44%) - LeetSync

---
+++ Google-Interview-Preparation_PREMIUM/739-daily-temperatures/daily-temperatures.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        int n = temp.size();
+        stack<pair<int,int>> st;
+        vector<int> ans(n, 0);
+
+        for(int i = n - 1; i >= 0; i--) {
+            if(i == n - 1) st.push({temp[i], i});
+            else {
+                while(!st.empty() && st.top().first <= temp[i]) {
+                    st.pop();
+                }
+                if(!st.empty() && st.top().first > temp[i]) {
+                    ans[i] = st.top().second - i ;
+                } else {
+                    ans[i] = 0;
+                }
+                st.push({temp[i], i});
+            }   
+        }
+
+        return ans;
+    }
+};

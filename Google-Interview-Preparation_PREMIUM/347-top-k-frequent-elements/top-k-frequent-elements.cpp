de3abed59076c61fb9880628a7b248e1f9fabc7e
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2025-12-23 14:20:28 +0530

    Time: 0 ms (100.00%) | Memory: 20 MB (10.64%) - LeetSync

--- Google-Interview-Preparation_PREMIUM/347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ Google-Interview-Preparation_PREMIUM/347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -6,22 +6,24 @@ public:
         int n = nums.size();
         
         unordered_map<int,int> mp;
-        multimap<int,int, greater<int>> mp2;
 
         for(auto num : nums) {
             mp[num]++;
         }
 
+        vector<vector<int>> bucket(n + 1);
+
         for(auto it = mp.begin(); it != mp.end(); it++) {
-            mp2.insert({it -> second, it -> first});
+            bucket[it->second].push_back(it->first);
         }
 
         vector<int> ans;
-        for(auto it = mp2.begin(); it != mp2.end(); it++) {
-            cout << it->first << " : " << it->second << endl;
-            ans.push_back(it -> second);
-            k--;
-            if(k == 0) return ans;
+        for(int i = n; i >= 1; i--) {
+            for(auto el: bucket[i]) {
+                ans.push_back(el);
+                k--;
+                if(k == 0) return ans;
+            }
         }
 
         return ans;

b56efe504d9f612594a255bf0489f528c68634e1
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2025-12-23 13:37:01 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (11.02%) - LeetSync

--- Google-Interview-Preparation_PREMIUM/242-valid-anagram/valid-anagram.cpp
+++ Google-Interview-Preparation_PREMIUM/242-valid-anagram/valid-anagram.cpp
@@ -3,17 +3,34 @@ public:
     bool isAnagram(string s, string t) {
         // bruteforce - sort and compare
 
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
 
-        int n = s.size(), m = t.size();
+        // int n = s.size(), m = t.size();
 
-        if(n != m) return false;
+        // if(n != m) return false;
 
-        for(int i = 0; i < n; i++) {
-            if(s[i] != t[i]) return false;
+        // for(int i = 0; i < n; i++) {
+        //     if(s[i] != t[i]) return false;
+        // }
+
+        // return true;
+
+        // optimal- use hashmap 
+
+        unordered_map<char, int> mp;
+        for(auto ch: s) {
+            mp[ch]++;
         }
 
+        for(auto ch: t) {
+            mp[ch]--;
+            if(mp[ch] < 0) return false;
+        }
+
+        for(char ch = 'a'; ch <= 'z'; ch++) {
+            if(mp[ch] > 0) return false;
+        }
         return true;
     }
 };

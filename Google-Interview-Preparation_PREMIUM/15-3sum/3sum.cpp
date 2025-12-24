a23023668ed5cf9c7f4da292fccb24edcdabbcbd
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2025-12-24 17:29:54 +0530

    Time: 47 ms (67.43%) | Memory: 28.9 MB (89.52%) - LeetSync

--- Google-Interview-Preparation_PREMIUM/15-3sum/3sum.cpp
+++ Google-Interview-Preparation_PREMIUM/15-3sum/3sum.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        set<vector<int>> ans;
-        
-        unordered_set<int> dups;
-        
-        unordered_map<int,int> seen;
-        
         int n = nums.size();
-        
-        for(int i = 0; i < n; i++) {
-            if(dups.insert(nums[i]).second) {
-                for(int j = i + 1; j < n; j++) {
-                    int complement = -nums[i] - nums[j];
-                    auto it = seen.find(complement);
-                    
-                    if(it != seen.end() && it->second == i) {
-                        vector<int> temp = {nums[i], nums[j], complement};
-                        
-                        sort(temp.begin(), temp.end());
-                        
-                        ans.insert(temp);
-                    }
-                    
-                    seen[nums[j]] = i;
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> ans;
+
+        for(int i = 0; i < n - 2; i++) {
+
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while(j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if(sum == 0){
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    while(j < k && nums[j] == nums[j+1]) j++;
+                    while(j < k && nums[k] == nums[k-1]) k--;
+                    j++;
+                    k--;
                 }
+                else if(sum < 0) j++;
+                else k--;
             }
         }
-        
-        return vector<vector<int>>(ans.begin(), ans.end());
+
+        return ans;
     }
 };

dc3c58edb3e0d1ea2ff76b67f91ea6ee01c43ffb
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2025-12-25 00:37:51 +0530

    Time: 17 ms (80.39%) | Memory: 139.1 MB (64.18%) - LeetSync

--- Google-Interview-Preparation_PREMIUM/239-sliding-window-maximum/sliding-window-maximum.cpp
+++ Google-Interview-Preparation_PREMIUM/239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int i = 0, j = 0, n = nums.size();
+        int n = nums.size();
 
-        priority_queue<pair<int,int>> pq;
         vector<int> ans;
+        deque<int> dq;
 
-        while(j < n) {
-            while(j < n && j - i + 1 <= k) {
-                pq.push({nums[j], j});
-                j++;
-            }
+        for(int i = 0; i < n; i++) {
+            if(!dq.empty() && dq.front() <= (i - k)) dq.pop_front();
 
-            while(!pq.empty() && pq.top().second < i) pq.pop();
-            if(!pq.empty()) ans.push_back(pq.top().first);
-            i++;
+            while(!dq.empty() && nums[dq.back()] <= nums[i]) dq.pop_back();
+
+            dq.push_back(i);
+
+            if(i + 1 >= k)
+                ans.push_back(nums[dq.front()]);
         }
 
         return ans;

3ca4e082d727d6cd6366acd127e83ca5b466e9c4
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2025-12-24 17:46:06 +0530

    Time: 22 ms (19.77%) | Memory: 14.3 MB (20.49%) - LeetSync

--- Google-Interview-Preparation_PREMIUM/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ Google-Interview-Preparation_PREMIUM/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -2,24 +2,27 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int n = s.size();
-        int tail = 0, head = -1;
+
+        int ans = 0;
 
         unordered_map<char,int> mp;
 
-        int ans = 0;
-        while(head < n) {
-            while(head + 1 < n && mp[s[head+1]] < 1) {
+        int head = -1, tail = 0;
+
+        while(tail < n) {
+            while(head + 1 < n && mp.find(s[head + 1]) == mp.end()){
                 head++;
                 mp[s[head]]++;
             }
 
             ans = max(ans, head - tail + 1);
 
-            if(head < tail) {
-                tail = head + 1;
-                head = tail;
+            if(tail > head) {
+                tail++;
+                head = tail - 1;
             } else {
                 mp[s[tail]]--;
+                if(mp[s[tail]] == 0) mp.erase(s[tail]);
                 tail++;
             }
         }

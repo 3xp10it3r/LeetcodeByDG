070f46e9d2c6da6661d1da771702a06a79ece2a9
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2025-12-25 02:40:07 +0530

    Time: 7 ms (63.81%) | Memory: 11.5 MB (41.31%) - LeetSync

--- Google-Interview-Preparation_PREMIUM/76-minimum-window-substring/minimum-window-substring.cpp
+++ Google-Interview-Preparation_PREMIUM/76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,55 +1,48 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        
-        int n = s.size();
         int len = t.size();
-        
+
+        int n = s.size();
+
         int head = -1, tail = 0;
+
+        int index = -1, length = 1e9;
         
-        int startIndex = -1;
-        int ansLength = 1e9;
-        
-        
-        //ds 
+        // ds
         unordered_map<char, int> mp;
-        
         int cnt = 0;
-        
-        //ds initial update
-        
-        for(auto &ch: t) {
+
+        for(auto& ch: t) {
             mp[ch]++;
         }
-        
-        while(head < n) {
+
+        while(tail < n) {
             while(head + 1 < n && cnt < len) {
                 head++;
-                
                 if(mp[s[head]] > 0) {
                     cnt++;
                 }
-                
                 mp[s[head]]--;
             }
-            
-            if(cnt == len && (head - tail + 1) < ansLength) {
-                startIndex = tail;
-                ansLength = head - tail + 1;
+
+            if(cnt == len && (head - tail + 1) < length){
+                index = tail;
+                length = head - tail + 1;
             }
-            
+
             if(tail > head) {
-                head = tail;
-                tail = head + 1;
+                tail++;
+                head = tail - 1;
             } else {
                 mp[s[tail]]++;
-                if(mp[s[tail]] > 0) {
+                if(mp[s[tail]] > 0)
                     cnt--;
-                }
                 tail++;
             }
         }
-        if(startIndex == -1) return "";
-        return s.substr(startIndex, ansLength);
+
+        if(index == -1) return "";
+        return s.substr(index, length);
     }
 };

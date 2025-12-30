9adfe0d719351f17d089015a2a4e8f0d30f6e51e
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2025-12-31 00:13:39 +0530

    Time: 67 ms (17.88%) | Memory: 21.6 MB (15.50%) - LeetSync

--- Google-Interview-Preparation_PREMIUM/45-jump-game-ii/jump-game-ii.cpp
+++ Google-Interview-Preparation_PREMIUM/45-jump-game-ii/jump-game-ii.cpp
@@ -1,26 +1,37 @@
 class Solution {
 public:
-    vector<int> dp;
-    int n;
+    // vector<int> dp;
+    // int n;
 
-    int rec(int level, vector<int>& nums) {
-        if(level == n-1) return 0;
-        if(level > n-1) return 1e9;
+    // int rec(int level, vector<int>& nums) {
+    //     if(level == n-1) return 0;
+    //     if(level > n-1) return 1e9;
 
-        if(dp[level] != -1) return dp[level];
+    //     if(dp[level] != -1) return dp[level];
 
-        int ans = 1e9;
-        for(int i = 1; i <= nums[level]; i++) {
-            ans = min(ans, 1 + rec(level + i, nums));
-        }
+    //     int ans = 1e9;
+    //     for(int i = 1; i <= nums[level]; i++) {
+    //         ans = min(ans, 1 + rec(level + i, nums));
+    //     }
 
-        return dp[level] = ans;
-    }
+    //     return dp[level] = ans;
+    // }
 
     int jump(vector<int>& nums) {
-        n = nums.size();
-        dp = vector(n + 1, -1);
+        int n = nums.size();
+        vector<int> dp(n+1, 1e9);
 
-        return rec(0, nums);
+        dp[n - 1] = 0; // at last index already there;
+
+        for(int i = n - 2; i >= 0; i--) {
+
+            for(int j = 1; j <= nums[i]; j++) {
+                if(i + j < n) {
+                    dp[i] = min(dp[i], 1 + dp[i + j]);
+                }
+            }
+        }
+
+        return dp[0];
     }
 };

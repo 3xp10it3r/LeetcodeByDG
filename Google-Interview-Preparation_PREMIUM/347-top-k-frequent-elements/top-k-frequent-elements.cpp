e7f48b36147aa8f9506398ce6e09d22de7686167
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2025-12-30 23:53:28 +0530

    Time: 6 ms (21.04%) | Memory: 17.9 MB (61.50%) - LeetSync

--- Google-Interview-Preparation_PREMIUM/347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ Google-Interview-Preparation_PREMIUM/347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        // use hashmap
-
-        int n = nums.size();
-        
         unordered_map<int,int> mp;
 
-        for(auto num : nums) {
-            mp[num]++;
-        }
+        for(auto num: nums) mp[num]++;
 
-        vector<vector<int>> bucket(n + 1);
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
 
-        for(auto it = mp.begin(); it != mp.end(); it++) {
-            bucket[it->second].push_back(it->first);
+        for(auto& [key, value] : mp) {
+            pq.push({value, key});
+            if(pq.size() > k) {
+                pq.pop();
+            }
         }
 
         vector<int> ans;
-        for(int i = n; i >= 1; i--) {
-            for(auto el: bucket[i]) {
-                ans.push_back(el);
-                k--;
-                if(k == 0) return ans;
-            }
+
+        while(!pq.empty()) {
+            ans.push_back(pq.top().second);
+            pq.pop();
         }
 
         return ans;

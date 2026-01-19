915632a27ec55761327149468a7db454c79a40b4
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2026-01-19 21:46:42 +0530

    Time: 112 ms (26.88%) | Memory: 26 MB (34.10%) - LeetSync

--- Google-Interview-Preparation_PREMIUM/5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ Google-Interview-Preparation_PREMIUM/5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,31 +1,51 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
+    vector<vector<bool>> dp;
 
-    bool checkPalindrome(int i, int j, string& s) {
-        if(i >= j) return true;
+    // bool checkPalindrome(int i, int j, string& s) {
+    //     if(i >= j) return true;
 
-        if(dp[i][j] != -1) return dp[i][j];
+    //     if(dp[i][j] != -1) return dp[i][j];
 
-        if(s[i] == s[j]){
-            return dp[i][j] = checkPalindrome(i+1, j-1, s);
-        }
+    //     if(s[i] == s[j]){
+    //         return dp[i][j] = checkPalindrome(i+1, j-1, s);
+    //     }
 
-        return dp[i][j] = false;
-    }
+    //     return dp[i][j] = false;
+    // }
 
     string longestPalindrome(string s) {
         int n = s.size();
 
-        dp = vector<vector<int>>(n, vector<int>(n, -1));
+        dp = vector<vector<bool>>(n, vector<bool>(n, false));
         int mxLen = 0;
         int startIndex = -1;
 
+        // for 1 length 
         for(int i = 0; i < n; i++) {
-            for(int j = i; j < n; j++) {
-                if(checkPalindrome(i, j, s)) {
-                    if(mxLen < j - i + 1) {
-                        mxLen = j - i + 1;
+            dp[i][i] = true;
+            mxLen = 1;
+            startIndex = i;
+        }
+
+        // for 2 length 
+        for(int i = 0; i + 1 < n; i++) {
+            if(s[i] == s[i+1]){
+                dp[i][i+1] = true;
+                mxLen = 2;
+                startIndex = i;
+            }
+        }
+
+        // for >= 3 length
+
+        for(int len = 3; len <= n; len++) {
+            for(int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+                if(s[i] == s[j] && dp[i + 1][j - 1]){
+                    dp[i][j] = true;
+                    if(len > mxLen) {
+                        mxLen = len;
                         startIndex = i;
                     }
                 }

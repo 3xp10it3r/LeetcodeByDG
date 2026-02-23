1fa48849aa159e9ac8ee036df942295ede00635d
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2026-02-23 21:49:23 +0530

    Time: 1027 ms (8.81%) | Memory: 92.1 MB (18.31%) - LeetSync

---
+++ 354-russian-doll-envelopes/russian-doll-envelopes.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void printj(vector<vector<int>>& envelopes) {
+        for(auto e : envelopes){
+            cout << e[0] << " " << e[1] << endl;
+        }
+    }
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        sort(envelopes.begin(), envelopes.end(), [](auto &a, auto &b){
+            if(a[0] == b[0]) return a[1] > b[1];
+            return a[0] < b[0]; 
+        });
+
+        printj(envelopes);
+
+        vector<int> lis;
+
+        for(auto &envelope: envelopes) {
+            int height = envelope[1];
+
+            if(lis.empty() || lis.back() < height) {
+                lis.push_back(height);
+            } else {
+                auto it = lower_bound(lis.begin(), lis.end(), height);
+                *it = height;
+            }
+        }
+
+        return lis.size();
+    }
+};

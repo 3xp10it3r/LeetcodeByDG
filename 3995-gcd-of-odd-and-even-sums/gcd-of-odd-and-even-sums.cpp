38c63b19b86cebe768637376455e33ff6d6cbe1a
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2026-07-16 00:12:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (46.67%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
+    int gcd(int a, int b){
+        if(b==0) return a;
+        return gcd(b, a%b);
+    }
     int gcdOfOddEvenSums(int n) {
-       return __gcd(n*n, n*(n+1));
+       return gcd(n*n, n*(n+1));
     }
 };
